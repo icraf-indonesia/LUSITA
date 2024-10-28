f0ca1a67b063200583e51e01a61aa60d97a85f5c
Dewi Bodro <dewikiswanibodro@gmail.com>
2024-10-28 10:30:33 +0700

    deploy folder

--- shiny/footer.R
+++
@@ -1,5 +0,0 @@
-argonFooter <- argonDashFooter(
-  argonFooterMenu(
-    argonFooterItem("@ICRAF, 2020", src = "http://www.worldagroforestry.org/")
-  )
-)
--- shiny/header.R
+++
@@ -1,6 +0,0 @@
-argonHeader <- argonDashHeader(
-  gradient = TRUE,
-  color = "primary",
-  separator = TRUE,
-  separator_color = "secondary"
-)
